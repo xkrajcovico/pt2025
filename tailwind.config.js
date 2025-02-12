07f7da9b981cb074c68a6caf698660c81ecb8caa
Ondrej Krajčovič <xkrajcovico@stuba.sk>
2025-02-12 16:11:51 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: theme=> ({
+      colors: {
+        'shrek-light-green-1': 'rgba(97, 127, 76, 1)',
+        'shrek-dark-green-1': 'rgba(28, 28, 7, 1)',
+        'shrek-dark-brown-1': 'rgba(105, 80, 42, 1)',
+      },
+      backgroundImage: theme => ({
         'hero-image': "url('img/header_image.jpg')",
         'overlay': "url('img/overlay.png'), url('img/header_image.jpg')",
       }),
@@ -23,7 +28,6 @@ module.exports = {
     fontFamily: {
       'raleway': ['Raleway', 'sans-serif'],
     },
-    
   },
   variants: {
     extend: {},
